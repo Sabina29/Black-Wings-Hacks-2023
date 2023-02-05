8f105662085f576b08ae6638ad60eed02e6e2714
sabina29 <sabinamiezah@gmail.com>
2023-02-04 20:09:46 -0500

    Scrapping current project to properly port new env

--- melamarket/webpage/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL']= 'sqlite:///melamarket.db'
-db= SQLAlchemy(app)
-
-from webpage import routes
--- melamarket/webpage/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, session, request, redirect, url_for
-from webpage import app, db
-
-@app.route('/')
-def home():
-    return "This is the homepage"
