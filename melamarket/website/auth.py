25dca54dd4faaaeb985a0876e4096cd35b3db0cf
sabina29 <sabinamiezah@gmail.com>
2023-02-05 09:27:56 -0500

    Commiting newly added html files

--- melamarket/website/auth.py
+++ melamarket/website/auth.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth= Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-    return "<p>Login</p>"
+@auth.route('/explore')
+def explore():
+    return render_template('explore.html', dress= "Green Dress", page= "@alexa_shops", text= "Buy", text1="Connect", nice= "I love it!")
 
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
+@auth.route('/your-posts')
+def your_posts():
+    return render_template('your-posts.html', text="pretty in pink<3", text1="loved it so much, i had to double it XD")
+
